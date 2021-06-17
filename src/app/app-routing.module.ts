d79b40369facc93deb68024da7188548b9d82606
christamsnow <69160111+christamsnow@users.noreply.github.com>
2021-06-17 19:11:00 -0400

    added practice component and css changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {BoardComponent} from './board/board.component';
+import {PracticeComponent} from './practice/practice.component';
 
 const routes: Routes = [
-  {path: '', component: BoardComponent}
+  {path: '', component: BoardComponent},
+  {path: 'practice', component: PracticeComponent}
 ];
 
 @NgModule({
